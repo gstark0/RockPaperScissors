283fa38576421d9acf10f0647a04b943d8b497e9
gstark0 <gstark0@icloud.com>
2020-08-01 13:00:28 +0200

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Greg Stark on 01/08/2020.
+//  Copyright © 2020 Gregory Stark. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
